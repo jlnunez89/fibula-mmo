8bf7fcac23949743c7eaaea92f94ce74464fa2d8
Jose L. Nunez <jlnunez89@gmail.com>
2019-09-15 16:57:35 -0700

    hello world with logging and basic validation helper (#8)

---
+++ OpenTibia.Server.Standalone/Program.cs
@@ -0,0 +1,96 @@
+﻿// -----------------------------------------------------------------
+// <copyright file="Program.cs" company="2Dudes">
+// Copyright (c) 2018 2Dudes. All rights reserved.
+// Author: Jose L. Nunez de Caceres
+// http://linkedin.com/in/jlnunez89
+//
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------
+
+namespace OpenTibia.Server.Standalone
+{
+    using System.IO;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using OpenTibia.Common.Helpers;
+    using Serilog;
+
+    /// <summary>
+    /// Class that represents a standalone OpenTibia server.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The cancellation token source for the entire application.
+        /// </summary>
+        private static readonly CancellationTokenSource MasterCancellationTokenSource = new CancellationTokenSource();
+
+        /// <summary>
+        /// The main entry point for the program.
+        /// </summary>
+        /// <param name="args">The arguments for this program.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public static async Task Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("logsettings.json")
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .Enrich.FromLogContext()
+                .CreateLogger();
+
+            Log.ForContext<Program>().Information("Building host...");
+
+            var host = new HostBuilder()
+                .ConfigureHostConfiguration(configHost =>
+                {
+                    configHost.SetBasePath(Directory.GetCurrentDirectory());
+                    configHost.AddJsonFile("hostsettings.json", optional: true, reloadOnChange: true);
+                    configHost.AddEnvironmentVariables(prefix: "OTS_");
+                    configHost.AddCommandLine(args);
+                })
+                .ConfigureAppConfiguration((hostContext, configApp) =>
+                {
+                    configApp.SetBasePath(Directory.GetCurrentDirectory());
+                    configApp.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                    configApp.AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                    configApp.AddEnvironmentVariables(prefix: "OTS_");
+                    configApp.AddCommandLine(args);
+                })
+                .ConfigureServices(ConfigureServices)
+                .UseSerilog()
+                .Build();
+
+            await host.RunAsync(MasterCancellationTokenSource.Token);
+        }
+
+        /// <summary>
+        /// Configuration root, where services are configured and added into the service collection, often depending on the configuration set.
+        /// </summary>
+        /// <param name="hostingContext">The hosting context.</param>
+        /// <param name="services">The services collection.</param>
+        private static void ConfigureServices(HostBuilderContext hostingContext, IServiceCollection services)
+        {
+            hostingContext.ThrowIfNull(nameof(hostingContext));
+            services.ThrowIfNull(nameof(services));
+
+            // Add the master cancellation token source of the entire service.
+            services.AddSingleton(MasterCancellationTokenSource);
+
+            // Add known instances of configuration and logger.
+            services.AddSingleton(hostingContext.Configuration);
+            services.AddSingleton(Log.Logger);
+
+            // TODO: Add service implementations here.
+            // Those executing should derive from IHostedService and be added using AddHostedService.
+            services.AddHostedService<SampleHostedService>();
+        }
+    }
+}
