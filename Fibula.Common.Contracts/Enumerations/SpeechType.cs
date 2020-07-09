2b6760ce494fad0eb3b60ccc3637a68bab522ea6
Jose Nunez de Caceres <jlnunez89@gmail.com>
2020-07-09 10:40:37 -0700

    ammended license

--- Fibula.Common.Contracts/Enumerations/SpeechType.cs
+++ Fibula.Common.Contracts/Enumerations/SpeechType.cs
@@ -1,12 +1,11 @@
 ﻿// -----------------------------------------------------------------
 // <copyright file="SpeechType.cs" company="2Dudes">
-// Copyright (c) 2018 2Dudes. All rights reserved.
-// Author: Jose L. Nunez de Caceres
-// jlnunez89@gmail.com
-// http://linkedin.com/in/jlnunez89
+// Copyright (c) | Jose L. Nunez de Caceres et al.
+// https://linkedin.com/in/nunezdecaceres
 //
-// Licensed under the MIT license.
-// See LICENSE.txt file in the project root for full license information.
+// All Rights Reserved.
+//
+// Licensed under the MIT License. See LICENSE in the project root for license information.
 // </copyright>
 // -----------------------------------------------------------------
 
