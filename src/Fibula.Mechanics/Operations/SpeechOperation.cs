1a592ef80fdf9ed1e50d2386c0767a3bcd1b49a8
Jose Nunez de Caceres <jlnunez89@gmail.com>
2020-08-20 02:13:46 -0700

    fix build- missed a line from another experimental branch

--- src/Fibula.Mechanics/Operations/SpeechOperation.cs
+++ src/Fibula.Mechanics/Operations/SpeechOperation.cs
@@ -107,15 +107,6 @@ namespace Fibula.Mechanics.Operations
 
                     return;
                 }
-
-                if (this.Content.StartsWith("!test"))
-                {
-                    var script = this.Content.Replace("!test", string.Empty).Trim();
-
-                    context.GameApi.TestScripting(script).Wait();
-
-                    return;
-                }
             }
             catch
             {
